cf64401708034672d1c17b12a7a2a9541a260a34
Alwan Alyafi Mulyawan <dev.alwanly@gmail.com>
2026-01-31 14:40:36 +0700

    chore: add initialize configuration

--- pkg/database/sqlite.go
+++ pkg/database/sqlite.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/Alwanly/service-distribute-management/internal/models"
 	"gorm.io/driver/sqlite"
@@ -34,5 +35,25 @@ func RunMigrations(db *gorm.DB) error {
 	if err := db.AutoMigrate(models...); err != nil {
 		return fmt.Errorf("failed to run migrations: %w", err)
 	}
+	return nil
+}
+
+func SeedInitialData(db *gorm.DB) error {
+	// Check if initial configuration exists
+	var count int64
+	if err := db.Model(&models.Configuration{}).Count(&count).Error; err != nil {
+		return fmt.Errorf("failed to check existing configurations: %w", err)
+	}
+
+	if count == 0 {
+		initialConfig := models.Configuration{
+			ETag:       fmt.Sprintf("%x-%d", 1, time.Now().UnixNano()),
+			ConfigData: "{}",
+		}
+		if err := db.Create(&initialConfig).Error; err != nil {
+			return fmt.Errorf("failed to seed initial configuration: %w", err)
+		}
+	}
+
 	return nil
 }
